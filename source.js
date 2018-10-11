7748d6c492da309d86c98013245d8b6e2509e26f
GeorgeCrisan <georgerdp@gmail.com>
2018-10-11 14:28:38 +0100

    some css and javascript

--- source.js
+++ source.js
@@ -1,6 +1,5 @@
 //check that my script, lodash and jquerty are ready
-
-console.log('ready',_,$);
+//console.log('ready',_,$);
 
 // declare vars
 
@@ -18,6 +17,25 @@ function clearLocations(){
    markers = [];
 }
 
+// helper function get geoCodeAddress
+function geoCodeAddress(address){
+     return new Promise((resolve,reject)=>{
+
+                geocoder.geocode({'address': address}, function(results,status){
+
+                     if(status == google.maps.GeocoderStatus.OK){
+
+                        resolve(results);
+
+                     } else {
+
+                       reject(status);
+
+                     }
+                });
+     });
+}
+
 //declare callback for google maps api located in script on index.html file
 
 const initMap = function(){
@@ -27,7 +45,7 @@ const initMap = function(){
   });
 
   markers = [];
-  geocoder = google.maps.Geocoder();
+  geocoder = new google.maps.Geocoder();
   infoWindow = new google.maps.InfoWindow();
   service = new google.maps.places.PlacesService(map);
 }
@@ -36,12 +54,20 @@ const initMap = function(){
 
 document.getElementById('searchButton').addEventListener('click',(event)=>{
    // event.preventDefault();
-   let searchTerm, location, radius;
+   let searchTerm, location, radius,country;
    searchTerm = document.getElementById('searchTerm').value;
    location = document.getElementById('location').value;
    radius = document.getElementById('mileRadius').value;
+   country = document.getElementById('selectCountry').value;
    //validation if no value return false ... to add
-   console.log(searchTerm,location,radius);
+   console.log(searchTerm,typeof location,radius,`${country} ${location}`);
+
+
+   geoCodeAddress(`${location}, ${country}`).then((result)=>{
+     console.log(result,'from geo coder');
+   }).catch((status)=>console.log(status));
 
 });
+
+
 
